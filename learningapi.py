f8d56695910ffa5e1b7eb797ce2af63fd2864ee3
TeveredziSJ <58471235+MrWorldEnd@users.noreply.github.com>
2022-04-26 22:35:25 +0100

    test3

--- learningapi.py
+++ learningapi.py
@@ -1,4 +1,6 @@
+#createv env file
 import requests
+import datetime
 import base64
 
 client_id = 'f2483274d6744f639231ab05ac8714f7'
@@ -11,7 +13,7 @@ client_creds = f"{client_id}:{client_secret}"
 base64_client_creds = base64.b64encode(client_creds.encode())
 print(base64_client_creds)
 
-tocken_url =  'https://accounts.spotify.com/api/token'
+token_url =  'https://accounts.spotify.com/api/token'
 method = 'POST'
 token_data = {
     "grant_type" : "client_credentials"
@@ -21,6 +23,12 @@ tocken_header = {
     "Authorization" : f"Basic {base64_client_creds.decode()}"
 }
 
-r = requests.post(tocken_url, data = token_data, headers = token_header)
-
+r = requests.post(token_url, data = token_data, headers = token_header)
 print(r.json())
+tocken_response_data = r.json()
+
+now = datetime.datetime.now()
+access_tocken = tocken_response_data['access_token']
+expires_in = tocken_response_data['expires_in']
+expires = now + datetime.timedelta(seconds = expires_in)
+did_expire = expires < now
