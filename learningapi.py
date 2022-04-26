5e112e4f1ef7eef69240f0ea236bc80625aeb01d
TeveredziSJ <58471235+MrWorldEnd@users.noreply.github.com>
2022-04-26 23:00:50 +0100

    update 22.51

--- learningapi.py
+++ learningapi.py
@@ -7,6 +7,16 @@ client_id = 'f2483274d6744f639231ab05ac8714f7'
 client_secret = '518efa1a7b434a44a50e1c5e44674119'
 
 #do tocken lookup
+class SpotifyAPI(Object):
+    access_tocken = None
+    access_tocken_expires = datetime.datetime.now()
+    client_id = None
+    client_secret = None
+
+    def __init__(self, client_id, client_secret, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.client_id = client_id
+        self.client_secret = client_secret
 
 client_creds = f"{client_id}:{client_secret}"
 
@@ -25,10 +35,12 @@ tocken_header = {
 
 r = requests.post(token_url, data = token_data, headers = token_header)
 print(r.json())
-tocken_response_data = r.json()
+valid_request = r.status_code in range(200, 299)
 
-now = datetime.datetime.now()
-access_tocken = tocken_response_data['access_token']
-expires_in = tocken_response_data['expires_in']
-expires = now + datetime.timedelta(seconds = expires_in)
-did_expire = expires < now
+if valid_request:
+    token_response_data = r.json()
+    now = datetime.datetime.now()
+    access_tocken = token_response_data['access_token']
+    expires_in = token_response_data['expires_in']
+    expires = now + datetime.timedelta(seconds = expires_in)
+    did_expire = expires < now
